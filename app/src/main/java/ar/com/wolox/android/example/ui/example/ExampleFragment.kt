9ec2645248ee56799ea77369d6039b079b73738e
Pavlo Holota <pavlo.holota@wolox.com.ar>
2018-09-24 19:18:14 -0300

    commit 24 9

--- app/src/main/java/ar/com/wolox/android/example/ui/example/ExampleFragment.kt
+++ app/src/main/java/ar/com/wolox/android/example/ui/example/ExampleFragment.kt
@@ -1,5 +1,6 @@
 package ar.com.wolox.android.example.ui.example
 
+import android.content.Context
 import android.content.Intent
 import ar.com.wolox.android.R
 import ar.com.wolox.android.example.ui.viewpager.ViewpagerActivity
@@ -17,9 +18,28 @@ class ExampleFragment : WolmoFragment<ExamplePresenter>(), IExampleView {
     }
 
     override fun setListeners() {
-        vUsernameInput.onTextChanged { vLoginButton.isEnabled = it.isNotBlank() }
-        vLoginButton.onClickListener {
-            presenter.storeUsername(vUsernameInput.text.toString())
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
+        val vUsername = sharedPref.getString("Username", "")
+        val vPassword = sharedPref.getString("Password", "")
+
+        if (vUsername.isNotEmpty() && vPassword.isNotEmpty()) {
+            presenter.storeUsername(vUsername)
+        } else {
+            vUsernameInput.onTextChanged { vLoginButton.isEnabled = it.isNotBlank() }
+            vPasswordInput.onTextChanged { vLoginButton.isEnabled = it.isNotBlank() }
+            vLoginButton.onClickListener {
+                if (vUsernameInput.text.toString().isEmpty() || vPasswordInput.text.toString().isEmpty()) {
+                    vUsernameInput.setError("Todos los campos son obligatorios.")
+                } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(vUsernameInput.text.toString()).matches()) {
+                    vUsernameInput.setError("Formato invalido, un ejemplo válido es example@domain.com")
+                } else {
+                    presenter.storeUsername(vUsernameInput.text.toString())
+
+                    sharedPref.edit().putString("Username", vUsernameInput.text.toString())
+                    sharedPref.edit().putString("Password", vPasswordInput.text.toString())
+                    sharedPref.edit().apply()
+                }
+            }
         }
     }
 
