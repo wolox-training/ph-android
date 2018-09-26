f641c90e0d8780e4e1946d1852cb2e019406445b
Pavlo Holota <pavlo.holota@wolox.com.ar>
2018-09-26 16:57:31 -0300

    se termino la etapa de validacion y persistencia del frontend

--- app/src/main/java/ar/com/wolox/android/example/ui/example/ExampleFragment.kt
+++ app/src/main/java/ar/com/wolox/android/example/ui/example/ExampleFragment.kt
@@ -15,32 +15,43 @@ class ExampleFragment : WolmoFragment<ExamplePresenter>(), IExampleView {
 
     override fun init() {
         vLoginButton.isEnabled = false
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
+
+        val vUserName = sharedPref.getString("Username", "")
+        if (vUserName != null && vUserName.isNotEmpty()) {
+            presenter.storeUsername(vUserName)
+        }
     }
 
     override fun setListeners() {
-        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
-        val vUsername = sharedPref.getString("Username", "")
-        val vPassword = sharedPref.getString("Password", "")
-
-        if (vUsername.isNotEmpty() && vPassword.isNotEmpty()) {
-            presenter.storeUsername(vUsername)
-        } else {
-            vUsernameInput.onTextChanged { vLoginButton.isEnabled = it.isNotBlank() }
-            vPasswordInput.onTextChanged { vLoginButton.isEnabled = it.isNotBlank() }
-            vLoginButton.onClickListener {
-                if (vUsernameInput.text.toString().isEmpty() || vPasswordInput.text.toString().isEmpty()) {
-                    vUsernameInput.setError("Todos los campos son obligatorios.")
-                } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(vUsernameInput.text.toString()).matches()) {
-                    vUsernameInput.setError("Formato invalido, un ejemplo válido es example@domain.com")
-                } else {
-                    presenter.storeUsername(vUsernameInput.text.toString())
-
-                    sharedPref.edit().putString("Username", vUsernameInput.text.toString())
-                    sharedPref.edit().putString("Password", vPasswordInput.text.toString())
-                    sharedPref.edit().apply()
-                }
+        vUsernameInput.onTextChanged { vLoginButton.isEnabled = it.isNotBlank() }
+        vPasswordInput.onTextChanged { vLoginButton.isEnabled = it.isNotBlank() }
+        vLoginButton.onClickListener {
+            if (validateFields()) {
+                saveUser()
+                presenter.storeUsername(vUsernameInput.text.toString())
             }
         }
+    }
+
+    private fun saveUser() {
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
+        with(sharedPref.edit()) {
+            putString("Username", vUsernameInput.text.toString())
+            sharedPref
+            commit()
+        }
+    }
+
+    private fun validateFields(): Boolean {
+        if (vUsernameInput.text.toString().isEmpty() || vPasswordInput.text.toString().isEmpty()) {
+            vUsernameInput.setError("Todos los campos son obligatorios.")
+            return false
+        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(vUsernameInput.text.toString()).matches()) {
+            vUsernameInput.setError("Formato invalido, un ejemplo válido es example@domain.com")
+            return false
+        }
+        return true
     }
 
     override fun onUsernameSaved() {
