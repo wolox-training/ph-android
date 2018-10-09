2eafab4eefd4f3b82bbe51a8373ec40e2d0b79c4
Pavlo Holota <pavlo.holota@wolox.com.ar>
2018-10-09 20:24:24 -0300

    'News' and 'Profile' views were created inside the 'Home' page. Minor front-end adjustmest pending.

--- app/src/main/java/ar/com/wolox/android/training/ui/home/HomeFragment.kt
+++ app/src/main/java/ar/com/wolox/android/training/ui/home/HomeFragment.kt
@@ -1,15 +1,34 @@
 package ar.com.wolox.android.training.ui.home
 
+import android.support.v4.app.Fragment
+import android.support.v4.util.Pair
 import ar.com.wolox.android.R
+import ar.com.wolox.wolmo.core.adapter.viewpager.SimpleFragmentPagerAdapter
 import ar.com.wolox.wolmo.core.fragment.WolmoFragment
 import ar.com.wolox.wolmo.core.presenter.BasePresenter
+import kotlinx.android.synthetic.main.fragment_home_main.*
 import javax.inject.Inject
 
 class HomeFragment @Inject constructor() : WolmoFragment<BasePresenter<Any>>() {
 
-    override fun layout(): Int = R.layout.fragment_home
+    @Inject internal lateinit var newsFragment: HomeNewsFragment
+    @Inject internal lateinit var profileFragment: HomeProfileFragment
+    private lateinit var fragmentPagerAdapter: SimpleFragmentPagerAdapter
+
+    override fun layout(): Int = R.layout.fragment_home_main
 
     override fun init() {
+        fragmentPagerAdapter = SimpleFragmentPagerAdapter(childFragmentManager)
+        fragmentPagerAdapter.addFragments(
+                Pair<Fragment, String>(newsFragment, newsFragment.title),
+                Pair<Fragment, String>(profileFragment, profileFragment.title))
+        vViewPager.adapter = fragmentPagerAdapter
+        vTabLayout.setupWithViewPager(vViewPager)
+        setTabIcons()
+    }
 
+    private fun setTabIcons(){
+        vTabLayout.getTabAt(0)?.setIcon(R.drawable.tab_selector_news)
+        vTabLayout.getTabAt(1)?.setIcon(R.drawable.tab_selector_profile)
     }
 }
